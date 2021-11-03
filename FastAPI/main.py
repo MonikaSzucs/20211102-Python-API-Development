8cc189e77efc488712646400cd3e358d5721ef3d
Monika <monika.silvia.s@hotmail.ca>
2021-11-03 14:55:26 -0700

    Using postman
    For get and post requests. Had to download postman for the desktop.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,4 +1,6 @@
+from types import BuiltinMethodType
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,3 +13,10 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    print(payLoad["title"])
+    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
